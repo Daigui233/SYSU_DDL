76c532cf6827e886cd210806796908344bd5980b
Daigui233 <824465870@qq.com>
2026-07-17 13:30:38 +0800

    新版本裁判系统

--- Master_RK3588S/setupUI/dist/setup_webui_restart.sh
+++ Master_RK3588S/setupUI/dist/setup_webui_restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 2
-kill -9 4744 2>/dev/null
+kill -9 40852 2>/dev/null
 sleep 1
-exec "/home/orangepi/Desktop/setupUI/dist/setup_webui"
+exec "/home/orangepi/Desktop/SYSU_DDL/Master_RK3588S/setupUI/dist/setup_webui"
